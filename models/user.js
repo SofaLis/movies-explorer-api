3afc5dcdb3c045b19e01833c7dccd2bd069ff1ee
Sofa Lis <sofyaocherednaya@yandex.ru>
2022-10-17 15:00:59 +0300

    feat: add models and functional files

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const isEmail = require('validator/lib/isEmail');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Неправильный формат почты',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+// eslint-disable-next-line func-names
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
